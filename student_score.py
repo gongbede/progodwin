168959807a13ab734e43c98541772b86ae91c00a
progodwin <godwinngbede19@.com>
2026-09-01 22:48:53 +0100

    student_score.py

--- student_score.py
+++ student_score.py
@@ -1,9 +1,14 @@
-school_score = 50
 student_score = 75
 
-if student_score >= student_score:
-    print("passes")
-    
+if student_score >= 70:
+    print("A")
+
+elif student_score >= 60:
+    print("B")
+
+elif student_score >= 50:
+    print("C")
+
 else:
-    print("fail")
+    print("F")
 

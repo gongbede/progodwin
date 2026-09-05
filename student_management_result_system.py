dfee4676faa1481f6f0237ee61ee5212ea0feb0b
progodwin <godwinngbede19@gmail.com>
2026-09-05 15:45:57 +0100

    student_management_result.py

--- student_management_result_system.py
+++ student_management_result_system.py
@@ -1,27 +1,34 @@
+def calculate_total(scores):
+    total = 0
+    for number in range(len(scores)):
+        total = total + scores[number]
+    return total
+
 students = ["Godwin", "John", "Peter", "David", "Michael"]
 scores = [85, 45, 72, 38, 90]
 
 pass_student_count = 0
 fail_student_count = 0
 
-total_scores = 0
 
 for number in range(len(students)):
     if scores[number] >= 50:
         print(students[number], scores[number], "Passed")
         pass_student_count = pass_student_count + 1
 
-        total_scores = total_scores + scores[number]
 
 
     else:
         print(students[number], scores[number], "Failed")
         fail_student_count = fail_student_count + 1
 
-        total_scores = total_scores + scores[number]
 
 
 
 print(pass_student_count, "Total passed")
 print(fail_student_count, "Total Failed")
-print(total_scores)
+
+
+
+total_scores = calculate_total(scores)
+print("Total Scores:",    total_scores)
